dd334749bbd6b9d70502936e6a9f3fa1c97a0ca4
Unknown <dennis.horstkamp@a-gentur.de>
2018-06-07 10:53:01 +0200

    firsttry

---
+++ APITest1/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using APITest1.Models;
+using RestSharp;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace APITest1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+       
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            //var key = "Authorization";
+            //var password = "565ec012251f932ea4000001d6578ace24c6409a64699d054cc0d4e4";
+            //var baseUrl = "http://api.football-api.com/2.0/";
+            //var tag = "team/9002";
+
+
+
+            //var rClient = new RestClient(baseUrl + tag + "?" + key + "=" + password);
+            //var rRequest = new RestRequest(Method.GET);
+
+            //IRestResponse rResponse = rClient.Execute(rRequest);
+
+            //var content = rResponse.Content;
+
+            //rClient.ExecuteAsync<List<RootObject>>(rRequest, response =>
+            //{
+            //    response.Data.ForEach(squad =>
+            //    {
+            //        List <T>() = new List(squad.name);
+            //        lvi.Tag = squad;
+            //        listViewName.BeginInvoke((Action)delegate { listViewName.Items.Add(lvi); });
+            //    });
+            //    listViewName.BeginInvoke((Action)delegate { listViewName.Enabled = true; });
+            //});
+
+           return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        const string apiKey = "5fa7531326154d49baa40543e595da31";
+    }
+}
---
+++ APITest1/Models/ManschaftStats.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITest1.Models
+{
+    public class Squad
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string number { get; set; }
+        public string age { get; set; }
+        public string position { get; set; }
+        public string injured { get; set; }
+        public string minutes { get; set; }
+        public string appearences { get; set; }
+        public string lineups { get; set; }
+        public string substitute_in { get; set; }
+        public string substitute_out { get; set; }
+        public string substitutes_on_bench { get; set; }
+        public string goals { get; set; }
+        public string assists { get; set; }
+        public string yellowcards { get; set; }
+        public string yellowred { get; set; }
+        public string redcards { get; set; }
+    }
+
+    public class Sidelined
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string startdate { get; set; }
+        public string enddate { get; set; }
+        public string id { get; set; }
+    }
+
+    public class TransfersIn
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string from_team { get; set; }
+        public string team_id { get; set; }
+        public string type { get; set; }
+    }
+
+    public class TransfersOut
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string to_team { get; set; }
+        public string team_id { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Statistic
+    {
+        public string rank { get; set; }
+        public string wins { get; set; }
+        public string wins_home { get; set; }
+        public string wins_away { get; set; }
+        public string draws { get; set; }
+        public string draws_home { get; set; }
+        public string draws_away { get; set; }
+        public string losses { get; set; }
+        public string losses_home { get; set; }
+        public string losses_away { get; set; }
+        public string clean_sheets { get; set; }
+        public string clean_sheets_home { get; set; }
+        public string clean_sheets_away { get; set; }
+        public string goals { get; set; }
+        public string goals_home { get; set; }
+        public string goals_away { get; set; }
+        public string goals_conceded { get; set; }
+        public string goals_conceded_home { get; set; }
+        public string goals_conceded_away { get; set; }
+        public string avg_goals_per_game_scored { get; set; }
+        public string avg_goals_per_game_scored_home { get; set; }
+        public string avg_goals_per_game_scored_away { get; set; }
+        public string avg_goals_per_game_conceded { get; set; }
+        public string avg_goals_per_game_conceded_home { get; set; }
+        public string avg_goals_per_game_conceded_away { get; set; }
+        public string avg_first_goal_scored { get; set; }
+        public string avg_first_goal_scored_home { get; set; }
+        public string avg_first_goal_scored_away { get; set; }
+        public string avg_first_goal_conceded { get; set; }
+        public string avg_first_goal_conceded_home { get; set; }
+        public string avg_first_goal_conceded_away { get; set; }
+        public string failed_to_score { get; set; }
+        public string failed_to_score_home { get; set; }
+        public string failed_to_score_away { get; set; }
+        public object biggest_defeat { get; set; }
+        public object biggest_defeat_home { get; set; }
+        public object biggest_defeat_away { get; set; }
+        public string scoring_minutes_0_15_pct { get; set; }
+        public string scoring_minutes_0_15_cnt { get; set; }
+        public string scoring_minutes_15_30_pct { get; set; }
+        public string scoring_minutes_15_30_cnt { get; set; }
+        public string scoring_minutes_30_45_pct { get; set; }
+        public string scoring_minutes_30_45_cnt { get; set; }
+        public string scoring_minutes_45_60_pct { get; set; }
+        public string scoring_minutes_45_60_cnt { get; set; }
+        public string scoring_minutes_60_75_pct { get; set; }
+        public string scoring_minutes_60_75_cnt { get; set; }
+        public string scoring_minutes_75_90_pct { get; set; }
+        public string scoring_minutes_75_90_cnt { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string team_id { get; set; }
+        public string is_national { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public string founded { get; set; }
+        public string leagues { get; set; }
+        public string venue_name { get; set; }
+        public string venue_id { get; set; }
+        public string venue_surface { get; set; }
+        public string venue_address { get; set; }
+        public string venue_city { get; set; }
+        public string venue_capacity { get; set; }
+        public string coach_name { get; set; }
+        public string coach_id { get; set; }
+        public List<Squad> squad { get; set; }
+        public List<Sidelined> sidelined { get; set; }
+        public List<TransfersIn> transfers_in { get; set; }
+        public List<TransfersOut> transfers_out { get; set; }
+        public List<Statistic> statistics { get; set; }
+    }
+}
