a5320a8446be993ca05ba0c7e98ec37ae8b21482
Dennis <dennis.horstkamp@a-gentur.de>
2018-06-07 12:56:20 +0200

    Add Controller
    SquadController added and Contact.cshtml updated

---
+++ APITest1/Controllers/SquadController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APITest1.Models;
+using System.Net;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Web.Http;
+
+namespace APITest1.Controllers
+{
+    
+    public class SquadController : ApiController
+    {
+        Squad[] squads = new Squad[]
+        {
+            new Squad { id = "1", name = "Mario Götze", age = "25", position = "Mittelfeld"},
+            new Squad { id = "2", name = "Marco Reus", age = "27", position = "Linksaußen"},
+            new Squad { id = "3", name = "Jaden Sancho", age ="21", position = "Mittelfeld"}
+        };
+
+        public IEnumerable<Squad> GetSquads()
+        {
+            return squads;
+        }
+
+        public IHttpActionResult GetSquad(string id)
+        {
+            var squad = squads.FirstOrDefault((s) => s.id == id);
+            if (squads== null)
+            {
+                return NotFound();
+            }
+            return Ok(squads);
+        }
+    }
+}
